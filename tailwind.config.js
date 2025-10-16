73db188b0bb3ec2bc5b4870b643a0700492e7059
davidvensilinr <relaxbro2005@gmail.com>
2025-10-16 16:21:48 +0530

    web title,description,icon,tailwindcss config correction

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,20 @@ const config = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: 'var(--color-background)',
-        foreground: 'var(--color-foreground)',
+        background: {
+          DEFAULT: 'var(--color-background)',
+        },
+        foreground: {
+          DEFAULT: 'var(--color-foreground)',
+        },
       },
       fontFamily: {
-        sans: ['var(--font-sans)'],
-        mono: ['var(--font-mono)'],
+        sans: ['var(--font-sans)', 'sans-serif'],
+        mono: ['var(--font-mono)', 'monospace'],
       },
     },
   },
