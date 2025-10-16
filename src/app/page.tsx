ef47055fb058bea27656861df27352c12679a38f
davidvensilinr <relaxbro2005@gmail.com>
2025-10-16 16:18:21 +0530

    Basic home page setup and add product page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <button className="btn btn-warning">error</button>
+      <h1 className="font-mono text-2xl italic font-normal tracking-widest text-center">Welcome to <span className="font-serif decoration-white text-black">ZenCartopia</span> !!!</h1>
     </div>
   );
 }
